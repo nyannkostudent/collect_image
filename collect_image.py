4b7088af56176e1b59e2f0cb036a947fa7c8d987
nyannkostudent <kichisecreative@gmail.com>
2017-10-07 14:36:15 +0900

    collecting succeeded but only 1 of 10 picture collected

--- collect_image.py
+++ collect_image.py
@@ -15,7 +15,7 @@ import pickle
 import urllib
 import hashlib
 import argparse
-# import sha3
+import sha3
 
 # define constant values
 # URL of Bing Search API
@@ -25,12 +25,16 @@ API_URL = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search'
 TIMEOUT = 5
 
 # Max number of images to collect defined by Bing Image Search API v5.0.
-IMAGES_PER_REQUEST = 50
+IMAGES_PER_REQUEST = 10
 
 # This func returns path to collected images folder.
 def get_image_folder():
     path_to_current_directory = os.path.dirname( os.path.abspath(__file__))
     path_to_image_directory = path_to_current_directory + '/media/'
+
+    # If directory don't exists, make directory.
+    if not os.path.isdir(path_to_image_directory):
+        os.mkdir(path_to_image_directory)
     return path_to_image_directory
 
 # This func parse commandline arguments.
@@ -62,6 +66,81 @@ def make_request_params(query, offset):
     })
     return params
 
+# make path to save image at
+def make_image_path(dirpath, url):
+    file_extension = os.path.splitext(url)[-1]
+    
+    if file_extension.lower() in ('.jpg', '.jpeg', '.gif', '.png', '.bmp'):
+        # required encoding for hashed
+        encoded_url = url.encode('utf-8')
+        hashed_url = str(hashlib.sha3_256(encoded_url).hexdigest())
+        file_name = hashed_url.replace('\'', '')
+        full_path = os.path.join(dirpath, hashed_url + hashed_url)
+
+        return full_path
+
+    else:
+        raise ValueError('not applicable file extension')
+
+
+def download_image(url):
+    response = requests.get(url, allow_redirects=True, timeout=TIMEOUT)
+    if response.status_code != 200:
+        error = Exception("HTTP status: " + response.status_code)
+        raise error
+    
+    content_type = response.headers["content-type"]
+    if 'image' not in content_type:
+        error = Exception('Content-Type : ' + content_type)
+        raise error
+    
+    return response.content
+
+def save_image(filepath, image):
+    with open(filepath, "wb") as fout:
+        fout.write(image)
+
+# request with header and params
+def request_images_and_save(header, params):
+    url_list = []
+
+    try:
+        # connection = http.client.HTTPSConnection('api.cognitive.microsoft.com')
+        # connection.request("POST", "/bing/v5.0/images/search?%s" % params, "{body}", header)
+        # response = connection.getresponse()
+        # data = response.read()
+
+        res = requests.get('https://api.cognitive.microsoft.com/bing/v5.0/images/search', headers=header, params=params)
+
+        # connection.close()
+    except:
+        raise Exception('connecting failed')
+    else:
+        # decoded_response = data.decode('utf-8')
+        data = res.json()
+        
+        pattern = r"&r=(http.+)&p="
+
+        for values in data['value']:
+            unquoted_url = urllib.parse.unquote(values['contentUrl'])
+            image_url = re.search(pattern, unquoted_url)
+
+            if image_url:
+                url_list.append(image_url.group(1))
+    
+    for url in url_list:
+        try:
+            print(url)
+            image_path = make_image_path(get_image_folder(), url)
+            print(image_path)
+            image = download_image(url)
+            save_image(image_path, image)
+        except Exception as err:
+            print("%s" % (err))
+            continue
+
+        return
+
 
 # If this file is executed from commandline, below script will execute.
 if __name__ == '__main__':
@@ -71,12 +150,16 @@ if __name__ == '__main__':
     # make request header
     header = make_request_header(args.API_key)
 
-    for i in range(args.count // IMAGES_PER_REQUEST):
+    for i in range(args.num_of_image // IMAGES_PER_REQUEST):
 
         # offset is the number of images to skip before returning images
         offset = i * IMAGES_PER_REQUEST
+        
         # make request params
         params = make_request_params(args.query, offset)
+        
+        request_images_and_save(header, params)
+        
 
         
     
